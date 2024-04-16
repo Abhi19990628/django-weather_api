e4f5243e891087a4ee43a3c2692d2b1f5d9eee82
abhihek kumar <abhbiv5976@gmail.com>
2024-04-17 02:35:48 +0530

    weather application using api

---
+++ main/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+import json  # import json to load json data to python dictionary
+import urllib.request  # urllib.request to make a request to api
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+
+        ''' api key might be expired use your own api_key
+            place api_key in place of appid ="your_api_key_here "  '''
+        # source contain JSON data from API
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=718bfb40fec2a75aee7767677f367613').read()
+                                        #https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+        # converting JSON data to a dictionary
+        list_of_data = json.loads(source)
+
+        # data for variable list_of_data
+        data = {
+            'city': city,
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ' '
+                        + str(list_of_data['coord']['lat']),
+            "temp": (int(list_of_data['main']['temp'])-273),
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+        }
+        print(data)
+    else:
+        data ={}
+    return render(request, "index.html", data)
